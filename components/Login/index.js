75671d52a724d051a886bf1945546466f970c9ca
Edgar Mueller <edmue@Edgars-MBP.fritz.box>
2019-08-22 00:26:55 +0200

    Refactork storage of client login

--- components/Login/index.js
+++ components/Login/index.js
@@ -2,55 +2,83 @@ import { Mutation, ApolloConsumer, Query } from "react-apollo";
 import gql from "graphql-tag";
 import GoogleLogin from "react-google-login";
 import { allLinks } from "../AllLinks";
+import { Fragment } from "react";
 
 const responseGoogle = response => {};
 
+const IS_LOGGED_IN = gql`
+  query {
+    userStatus @client {
+      isLoggedIn
+    }
+  }
+`;
+
 function Login() {
   return (
-    <Query query={allLinks}>
-      {({ loading, error }) => {
-        return (
-          <Mutation mutation={signInGoogle}>
-            {(authGoogle, { loading, error }) => {
-              return (
-                <ApolloConsumer>
-                  {client => {
-                    return (
-                      <GoogleLogin
-                        clientId={process.env.GOOGLE_CLIENT_ID}
-                        buttonText="Login"
-                        onSuccess={async resp => {
-                          if (resp.Zi.access_token) {
-                            const res = await authGoogle({
-                              variables: { token: resp.Zi.access_token }
-                            });
-                            localStorage.setItem(
-                              "x-token",
-                              res.data.authGoogle.token
-                            );
-                            const data = await client.query({
-                              query: allLinks
-                            });
-                            client.writeQuery({
-                              query: allLinks,
-                              data: { ...data.data }
-                            });
-                            client.reFetchObservableQueries();
-                          }
-                        }}
-                        onFailure={responseGoogle}
-                        cookiePolicy={"single_host_origin"}
-                      />
-                    );
-                  }}
-                </ApolloConsumer>
-              );
-            }}
-          </Mutation>
-        );
-        return null;
-      }}
-    </Query>
+    <Fragment>
+      <Query query={allLinks}>
+        {({ loading, error }) => {
+          return (
+            <Mutation mutation={signInGoogle}>
+              {(authGoogle, { loading, error }) => {
+                return (
+                  <ApolloConsumer>
+                    {client => {
+                      return (
+                        <GoogleLogin
+                          clientId={process.env.GOOGLE_CLIENT_ID}
+                          buttonText="Login"
+                          onSuccess={async resp => {
+                            if (resp.Zi.access_token) {
+                              const res = await authGoogle({
+                                variables: { token: resp.Zi.access_token }
+                              });
+                              localStorage.setItem(
+                                "x-token",
+                                res.data.authGoogle.token
+                              );
+                              const data = await client.query({
+                                query: allLinks
+                              });
+                              client.writeQuery({
+                                query: allLinks,
+                                data: { ...data.data }
+                              });
+                              client.writeData({
+                                data: {
+                                  userStatus: {
+                                    __typename: "UserStatus",
+                                    isLoggedIn: true
+                                  }
+                                }
+                              });
+                              client.reFetchObservableQueries();
+                            }
+                          }}
+                          onFailure={responseGoogle}
+                          cookiePolicy={"single_host_origin"}
+                        />
+                      );
+                    }}
+                  </ApolloConsumer>
+                );
+              }}
+            </Mutation>
+          );
+          return null;
+        }}
+      </Query>
+      <Query query={IS_LOGGED_IN}>
+        {({ data }) => (
+          <div>
+            {data.userStatus && data.userStatus.isLoggedIn
+              ? "connected"
+              : "nope"}
+          </div>
+        )}
+      </Query>
+    </Fragment>
   );
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,32 @@ import { ApolloProvider } from "react-apollo";
 import withApollo from "../lib/withApollo";
 
 class TakedownApp extends App {
+  UNSAFE_componentWillMount() {
+    if (process.browser) {
+      this.props.apollo.cache.writeData({
+        data: {
+          userStatus: {
+            __typename: "UserStatus",
+            isLoggedIn: localStorage.getItem("x-token") != null
+          }
+        }
+      });
+    }
+  }
+
+  componentDidUpdate() {
+    if (process.browser) {
+      this.props.apollo.cache.writeData({
+        data: {
+          userStatus: {
+            __typename: "UserStatus",
+            isLoggedIn: localStorage.getItem("x-token") != null
+          }
+        }
+      });
+    }
+  }
+
   render() {
     const { Component, pageProps, apollo } = this.props;
 
