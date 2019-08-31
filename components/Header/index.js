b6e30f06f0bdbb84394a4664717a66941786e18a
Edgar Mueller <edmue@Edgars-MBP.fritz.box>
2019-08-31 14:21:46 +0200

    Move Submit component

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,14 +1,10 @@
-import Link from "next/link";
 import { withRouter } from "next/router";
 import { Container, LinkText } from "./styles";
 import Login from "../Login";
-import Logout from "../Logout";
-import Submit from "../Submit";
 
 const Header = ({ router: { pathname } }) => (
   <Container>
     <Login />
-    <Submit />
   </Container>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Main from "../lib/layout";
 import Header from "../components/Header";
 import AllLinks from "../components/AllLinks";
+import Submit from "../components/Submit";
 
 export default () => (
   <Main>
     <Header />
+    <Submit />
     <AllLinks />
   </Main>
 );
