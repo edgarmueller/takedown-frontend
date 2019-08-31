7addc48a4536188585f423c38f9f196d7a488ef5
Edgar Mueller <edmue@Edgars-MBP.fritz.box>
2019-08-31 14:20:57 +0200

    Add ant design

--- next.config.js
+++ next.config.js
@@ -6,8 +6,28 @@ const Dotenv = require("dotenv-webpack");
 
 /* Without CSS Modules, with PostCSS */
 module.exports = withCSS({
-  target: 'serverless',
-  webpack: config => {
+  target: "serverless",
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style\/css.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === "function") {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === "function" ? [] : origExternals)
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: "null-loader"
+      });
+    }
     config.plugins = config.plugins || [];
 
     config.plugins = [
