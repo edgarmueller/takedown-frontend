064a62f1236ef0d7abee3ad649f095044ee6a8aa
Edgar Mueller <edgar.m.mueller@gmail.com>
2019-09-06 21:50:26 +0200

    Remove Submit from pages/index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Main from "../lib/layout";
 import Header from "../components/Header";
 import AllLinks from "../components/AllLinks";
-import Submit from "../components/Submit";
 
 export default () => (
   <Main>
     <Header />
-    <Submit />
     <AllLinks />
   </Main>
 );
