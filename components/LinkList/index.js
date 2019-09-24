e6888c8b43398b923340274c951d14e975f45397
Edgar Mueller <emueller@eclipsesource.com>
2019-09-24 11:09:24 +0200

    Change List item layout to vertical

--- components/LinkList/index.js
+++ components/LinkList/index.js
@@ -2,11 +2,7 @@ import LinkDetails from "../LinkDetails";
 import { List } from "antd";
 
 export default ({ links }) => (
-  <List
-    bordered
-    itemLayout="horizontal"
-    size="large"  
-  >
+  <List bordered itemLayout="vertical" size="large">
     {links.map(link => (<LinkDetails link={link} />))}
   </List>
 );
